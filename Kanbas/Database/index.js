e449e4a146322b1b12d4100a117326131ac56a11
jessicasu77 <jessica.su0807@gmail.com>
2024-03-27 11:21:25 -0400

    a5

---
+++ Kanbas/Database/index.js
@@ -0,0 +1,7 @@
+import courses from "./courses.js";
+import modules from "./modules.js";
+import assignments from "./assignments.js";
+import grades from "./grades.js";
+import enrollment from "./enrollment.js";
+import users from "./users.js";
+export default { courses , modules, assignments, grades, enrollment, users };
