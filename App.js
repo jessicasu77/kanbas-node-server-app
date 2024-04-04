a1bcec25df04e23d962d91bb6737751bb0482414
jessicasu77 <jessica.su0807@gmail.com>
2024-04-04 16:35:17 -0400

    a6 in progress

--- App.js
+++ App.js
@@ -4,12 +4,36 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import cors from "cors";
 import ModuleRoutes from './Kanbas/modules/routes.js';
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
-app.use(cors());
+app.use(
+ cors({
+   credentials: true,
+   origin: process.env.FRONTEND_URL
+ })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
-Hello(app)
+Hello(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000)
